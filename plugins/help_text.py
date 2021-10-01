b1584d419a0d02a9f9e245fdd97ad80e428d1d60
AJVAD <91082660+ajvadntr@users.noreply.github.com>
2021-10-01 16:03:28 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -75,7 +75,7 @@ async def about(bot, update):
         parse_mode="markdown",
         reply_to_message_id=update.message_id, 
         disable_web_page_preview = True, 
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😇 DEVELOPER", url="https://t.me/Trackstudio")]])  
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🧑‍💻DEVELOPER", url="https://t.me/ajvadntr")]])  
     )
 
 
@@ -91,9 +91,9 @@ async def rename_cb(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text="<b>File Name</b> : <code>{}</code> \n\nSelect the desired option below 😇".format(filename),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="📝 RENAME", callback_data="rename_button")],
-                                                [InlineKeyboardButton(text="⛔ CANCEL", callback_data="cancel_e")]]),
+        text="<b>File Name</b> : <code>{}</code> \n\nSelect the desired option below ".format(filename),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="RENAME", callback_data="rename_button")],
+                                                [InlineKeyboardButton(text="CANCEL", callback_data="cancel_e")]]),
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True   
@@ -104,5 +104,5 @@ async def cancel_extract(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text="Process Cancelled 🙃",
+        text="Process Cancelled",
     )
