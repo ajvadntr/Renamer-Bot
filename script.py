4e846afd3f9980563db950e7150d257c7c6ac627
AJVAD <91082660+ajvadntr@users.noreply.github.com>
2021-10-31 14:12:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,39 +1,37 @@
 class script(object):
 
 
-    START_TEXT = """** <b>👋Hi {}, 
+    START_TEXT = """** 👋Hi {}, 
 
 I'm A Simple File Renamer Bot With Permanent Thumbnail support!
 
 Send me any Telegram file and select rename option.
 
-Click /help for More Details....<b>
+Click /help for More Details....
 
 """
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
 
-    UPGRADE_TEXT = "CONTACT @Ajvadntr"
+    UPGRADE_TEXT = "CONTACT @ajvadntr"
 
     DOWNLOAD_START = "Download Started... "
 
     UPLOAD_START = "Uploading Started... "
 
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @AIOM_Rename_Bot**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me**"
 
-    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    SAVED_THUMB = "Thumbnail Saved ✅"
 
-    DEL_THUMB = "Thumbnail cleared succesfully!"
+    DEL_THUMB = "Thumbnail Cleared succesfully!"
 
-    NO_THUMB = "No thumbnails found!"
+    NO_THUMB = "No Thumbnails Found!"
 
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully"
 
-    CUSTOM_CAPTION_UL_FILE = "@AIOM_Rename_Bot"
+    CUSTOM_CAPTION_UL_FILE = ""
 
     HELP_USER = """
-
-<b>It's not that complicated😅</b>
     
 1. Send me a custom thumbnail (Optional) 
 
